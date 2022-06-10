1883552fa802291b5a6b261b58a5d80702e8d75f
A-Coding12 <a.codingorg@gmail.com>
2022-06-10 20:51:41 +0100

    Update Project Structure

---
+++ JavaScript/Script.js
@@ -0,0 +1,6 @@
+//show msg when user click on the button
+function showMsg() {
+    alert("Sorry This Web Not supported Backend yet!");
+}
+var myButton = document.getElementById("btn");
+myButton.addEventListener("click", showMsg);
